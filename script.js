2d9d7f3528117e02b2483e1627815bba0a9726b3
Joao Carvalho <joao12carvalho@hotmail.com>
2023-03-02 19:41:43 -0300

    Adicionado backgroud, e inserindo funcionalidades para o cliente receber mensagens de usuarios querendo agendar um horario

---
+++ script.js
@@ -0,0 +1,11 @@
+var modal = document.getElementById("modal");
+var btn = document.getElementById("budget");
+var closeBtn = document.getElementsByClassName("close")[0];
+
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+closeBtn.onclick = function() {
+  modal.style.display = "none";
+}
