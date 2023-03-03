f575a9f60deff640ff05bbb7314baf1cdd8af8da
Joao Carvalho <joao12carvalho@hotmail.com>
2023-03-03 16:15:37 -0300

    Ajustando bugs

--- script.js
+++ script.js
@@ -9,3 +9,28 @@ btn.onclick = function() {
 closeBtn.onclick = function() {
   modal.style.display = "none";
 }
+
+
+
+// EFEITO CAROUSEL NAS FOTOS
+$(".carousel").owlCarousel({
+    margin:5,
+    loop: true,
+    autoplay:true,
+    autplayTimeout:2000,
+    autoplayHoverpause:true,
+    responsive:{
+        0:{
+            items:1,
+            nav:false
+        },
+        600:{
+            items:3,
+            nav:false
+        },
+        1000:{
+            items:3,
+            nav:false
+        }
+    }
+});
